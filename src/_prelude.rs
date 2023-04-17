a08b3e8024e75aca8234737fc0be92a9644a29ac
ChousX <gmanenergy@gmail.com>
2023-04-17 03:27:22 -0400

    fully removed old files

--- src/_prelude.rs
+++
@@ -1,7 +0,0 @@
-pub use crate::utils::{check, Bindable, Binding, Key, ModKeyBindings};
-pub use crate::utils::{Error, Result};
-pub use bevy::prelude::*;
-pub use bevy_inspector_egui::prelude::*;
-pub use bevy_prototype_lyon::prelude::*;
-pub use derive_more::*;
-pub use enum_iterator::{all, Sequence};
--- src/_select/debug.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-use crate::prelude::*;
-
-pub struct SelectDebugPlugin;
-impl Plugin for SelectDebugPlugin {
-    fn build(&self, app: &mut App) {
-        app.register_type::<Selector>().register_type::<Selected>();
-    }
-}
